3e8c69e923d3e175ab7a86f41459769f3cc10b1e
Abdullah Faizi <abdullahmohammad530@u.boisestate.edu>
2022-10-23 20:20:55 -0600

    added more

--- excercsie.ts
+++ excercsie.ts
@@ -1,2 +1,7 @@
-let userName: string = "bob";
-let pageName: string | number = "1";
+const first = <T>(array: T[]) => {
+    return array[array.length - 1];
+};
+
+const f = first([1, 2, 3]);
+
+const f2 = first(["a", "b", "c"]);
